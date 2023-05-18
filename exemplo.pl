257fed3bdc6b0df27ab415ee904249be5d9b8f9c
Eduardo Dantas Luna <michaelthefear@gmail.com>
2023-05-17 21:08:51 -0300

    Coisas de prolog sobre rodar lista de regras

--- exemplo.pl
+++ exemplo.pl
@@ -5,6 +5,7 @@ resposta(A) :- random(0,2,X), ((A=s,X=1,!);(A=n,X=0,!)).
 ola :- resposta(A), 
         nl,
         write('Advinhe minha resposta, responderei sim ou nao? (s p/ sim, n para nao e q para sair)'),nl,
+        
         write('Nao esqueca de finalizar sua resposta com . ex: s. ou n.'),nl,
         read(X),
         nl,
--- teste.pl
+++ teste.pl
@@ -42,5 +42,27 @@ segundaEvolucao(rapidash).
 ultimaEvolucao(rapidash).
 cor(rapidash,amarelo).
 
+teste(X,[]) :- !.
+teste(X,[Rule|Rest]) :-
+    write('Testing rule: '), write(Rule), nl,
+    Z=call(Rule,X),
+    write('Result: '), write(Z), nl,
+    teste(X,Rest).
 
+% Define your rules here
+rule1 :- write('Rule 1 executed.\n').
+rule2 :- write('Rule 2 executed.\n').
+rule3 :- write('Rule 3 executed.\n').
 
+% Define a predicate to run all the rules in a list
+run_rules([]). % Base case: Empty list, no rules to run
+run_rules([Rule|Rest]) :-
+    call(Rule),    % Execute the current rule
+    run_rules(Rest). % Recursively call run_rules with the rest of the list
+
+% Example usage:
+run_all_rules :-
+    Rules = [rule1, rule2, rule3], % Define a list of rules
+    run_rules(Rules). % Call the predicate to run all rules
+
+[ultimaEvolucao,terceiraEvolucao]
